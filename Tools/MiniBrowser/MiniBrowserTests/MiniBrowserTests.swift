ace47587227e818ad1c5d13ad30f2c047c1bf2be
lynnswap <65545348+lynnswap@users.noreply.github.com>
2025-12-03 13:23:47 +0900

    Add MiniBrowser

---
+++ Tools/MiniBrowser/MiniBrowserTests/MiniBrowserTests.swift
@@ -0,0 +1,16 @@
+//
+//  MiniBrowserTests.swift
+//  MiniBrowserTests
+//
+//  Created by lynnswap on 2025/12/03.
+//
+
+import Testing
+
+struct MiniBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
