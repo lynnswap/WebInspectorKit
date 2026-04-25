560d8b74326c99c9869439e5add14d44783c789e
Kazuki Nakashima <65545348+lynnswap@users.noreply.github.com>
2026-04-25 20:56:55 +0900

    refactor(v2): split tab container controllers

---
+++ Sources/WebInspectorUI/V2/Tabs/V2_WICompactTabNavigationController.swift
@@ -0,0 +1,18 @@
+#if canImport(UIKit)
+import UIKit
+
+@MainActor
+final class V2_WICompactTabNavigationController: UINavigationController {
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+        view.backgroundColor = .clear
+        navigationBar.prefersLargeTitles = false
+        wiApplyClearNavigationBarStyle(to: self)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        nil
+    }
+}
+#endif
---
+++ Sources/WebInspectorUI/V2/Tabs/V2_WIRegularSplitRootViewController.swift
@@ -0,0 +1,48 @@
+#if canImport(UIKit)
+import UIKit
+
+@MainActor
+final class V2_WIRegularSplitRootViewController: UIViewController {
+    private let contentViewController: UIViewController
+
+    init(contentViewController: UIViewController) {
+        self.contentViewController = contentViewController
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        nil
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .clear
+        installContentViewController()
+    }
+
+    private func installContentViewController() {
+        guard contentViewController.parent == nil else {
+            return
+        }
+
+        addChild(contentViewController)
+        contentViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(contentViewController.view)
+        NSLayoutConstraint.activate([
+            contentViewController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            contentViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        contentViewController.didMove(toParent: self)
+        applyContentNavigationItem()
+    }
+
+    private func applyContentNavigationItem() {
+        navigationItem.leadingItemGroups = contentViewController.navigationItem.leadingItemGroups
+        navigationItem.trailingItemGroups = contentViewController.navigationItem.trailingItemGroups
+        navigationItem.additionalOverflowItems = contentViewController.navigationItem.additionalOverflowItems
+    }
+}
+#endif
