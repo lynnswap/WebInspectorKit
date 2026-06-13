7defc0b9244c9db7af90ce1202cdc78955eb4c86
Kazuki Nakashima <65545348+lynnswap@users.noreply.github.com>
2026-06-13 17:00:32 +0900

    refactor(dom): split session availability

--- Sources/WebInspectorCore/DOM/DOMSessionProtocolOperations.swift
+++ Sources/WebInspectorCore/DOM/DOMSessionProtocolOperations.swift
@@ -11,30 +11,6 @@ private struct TargetDestroyedEventParams: Decodable {
 }
 
 extension DOMSession {
-    package var canReloadDocument: Bool {
-        hasActiveCommandChannel && currentPageTargetID != nil
-    }
-
-    package var canBeginElementPicker: Bool {
-        hasActiveCommandChannel && currentPageTargetID != nil
-    }
-
-    package var canSelectElement: Bool {
-        hasActiveCommandChannel && currentPageRootNode != nil
-    }
-
-    package var canCopySelectedNodeText: Bool {
-        hasActiveCommandChannel && selectedNodeID != nil
-    }
-
-    package var canDeleteSelectedNode: Bool {
-        hasActiveCommandChannel && selectedNodeID != nil
-    }
-
-    private var hasActiveCommandChannel: Bool {
-        commandChannel?.acceptsActiveCommands == true
-    }
-
     package func bindProtocolChannel(
         _ commandChannel: ProtocolCommandChannel,
         recordError: @escaping (InspectorSessionError?) -> Void
