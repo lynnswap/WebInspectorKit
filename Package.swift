f0a7944059943260ba0a40c754d4bfb3d5900d3e
Kazuki Nakashima <65545348+lynnswap@users.noreply.github.com>
2026-07-06 01:13:20 +0900

    build(deps): update SyntaxEditorUI to 0.16.4

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/lynnswap/SyntaxEditorUI.git",
-            exact: "0.16.3"
+            exact: "0.16.4"
         ),
         .package(
             url: "https://github.com/p-x9/MachOKit.git",
